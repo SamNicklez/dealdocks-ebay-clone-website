876914cbde16d0ee0a9317caf5059191d5ac0c0d
Sam Loecke <sloecke@uiowa.edu>
2023-11-14 09:38:52 -0600

    Added a create and destroy method to the bookmarks controller clas

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class BookmarksController < ApplicationController
+
+  def create
+    item = Item.find(params[:item_id])
+    current_user.bookmarked_items << item
+    # Redirect or respond as appropriate
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    bookmark = current_user.bookmarks.find_by(item_id: item.id)
+    bookmark.destroy if bookmark
+    # Redirect or respond as appropriate
+  end
+
 end
