3f093a263060c9b9c6dea0c415a6807dd29905c4
Sam Loecke <sloecke@uiowa.edu>
2023-11-14 23:23:05 -0600

    Added logic into the search_controller for handling a search for the users bookmarked items

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,20 +3,23 @@ class SearchController < ApplicationController
 
   # Perform search and display results, with optional category filtering
   def index
-    # set up the min and max price for the price range
-
-    if params[:categories].nil? or params[:categories].empty?
-      @results = Item.search params[:search_term], Category.all.map(&:name)
+    if params[:bookmarks].present?
+      # only get the users bookmarked items
+      @results = current_user.bookmarked_items
     else
-      @results = Item.search(params[:search_term], params[:categories])
-    end
+      # set up the min and max price for the price range
+      if params[:categories].nil? or params[:categories].empty?
+        @results = Item.search params[:search_term], Category.all.map(&:name)
+      else
+        @results = Item.search(params[:search_term], params[:categories])
+      end
 
-    if params[:min_price].present?
-      @results = @results.where("price >= ?", params[:min_price])
+      if params[:min_price].present?
+        @results = @results.where("price >= ?", params[:min_price])
+      end
+      if params[:max_price].present?
+        @results = @results.where("price <= ?", params[:max_price])
+      end
     end
-    if params[:max_price].present?
-      @results = @results.where("price <= ?", params[:max_price])
-    end
-
   end
 end
