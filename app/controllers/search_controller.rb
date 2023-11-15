ebf229e141322d304560ccf23996db1d971628f6
Sam Loecke <sloecke@uiowa.edu>
2023-11-14 20:37:54 -0600

    Added filtering by categories when on the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,10 @@ class SearchController < ApplicationController
 
   # Perform search and display results, with optional category filtering
   def index
-    puts params[:search_term]
-    @results = Item.search(params[:search_term], params[:categories])
+    if params[:categories].nil? or params[:categories].empty?
+      @results = Item.search params[:search_term], Category.all.map(&:name)
+    else
+      @results = Item.search(params[:search_term], params[:categories])
+    end
   end
 end
