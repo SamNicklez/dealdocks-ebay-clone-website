64d7ea2e931fb592df4b056551c1785d6a19f777
Sam Loecke <sloecke@uiowa.edu>
2023-11-14 22:57:43 -0600

    Added a filter by price method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,20 @@ class SearchController < ApplicationController
 
   # Perform search and display results, with optional category filtering
   def index
+    # set up the min and max price for the price range
+
     if params[:categories].nil? or params[:categories].empty?
       @results = Item.search params[:search_term], Category.all.map(&:name)
     else
       @results = Item.search(params[:search_term], params[:categories])
     end
+
+    if params[:min_price].present?
+      @results = @results.where("price >= ?", params[:min_price])
+    end
+    if params[:max_price].present?
+      @results = @results.where("price <= ?", params[:max_price])
+    end
+
   end
 end
