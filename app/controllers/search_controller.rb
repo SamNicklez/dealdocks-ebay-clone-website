5fe54de4fcd11581717d39041b32ede3813bbd42
Sam Loecke <sloecke@uiowa.edu>
2023-11-15 17:58:25 -0600

    Rspec tests for the filtering in the search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,13 @@
 class SearchController < ApplicationController
   before_action :require_login
 
-  # Perform search and display results, with optional category filtering
   def index
-    # Start with all items or just bookmarked items based on the bookmarks param
-    if params[:bookmarks].present? and params[:bookmarks] == '1'
-      # Only get the user's bookmarked items
-      @results = current_user.bookmarked_items
-    else
-      @results = Item.all
-    end
+    @results = if params[:bookmarks].present? && params[:bookmarks] == '1'
+                 current_user.bookmarked_items
+               else
+                 Item.all
+               end
 
-    # Filter by categories if any are selected
     if params[:categories].present?
       @results = @results.joins(:categories).where(categories: { name: params[:categories] })
       @results = @results.search(params[:search_term], params[:categories])
@@ -19,27 +15,17 @@ class SearchController < ApplicationController
       @results = @results.search(params[:search_term], Category.all.map(&:name))
     end
 
-    unless @results.nil?
-      if params[:seller].present?
-        # Filter to only the specified seller
-        # Need to get the user ID from the username
-        seller = User.find_by(username: params[:seller])
-        if seller
-          @results = @results.where("items.user_id = ?", seller.id)
-        else
-          @results = Item.none
-        end
-      end
+    if params[:seller].present?
+      seller = User.find_by(username: params[:seller])
+      @results = seller ? @results.where("items.user_id = ?", seller.id) : Item.none
     end
 
-    unless @results.nil?
-      # Filter by price range if specified
-      if params[:min_price].present?
-        @results = @results.where("price >= ?", params[:min_price])
-      end
-      if params[:max_price].present?
-        @results = @results.where("price <= ?", params[:max_price])
-      end
+    if params[:min_price].present?
+      @results = @results.where("price >= ?", params[:min_price])
+    end
+
+    if params[:max_price].present?
+      @results = @results.where("price <= ?", params[:max_price])
     end
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -37,6 +37,28 @@ RSpec.describe SearchController, type: :controller do
     ]
   }
 
+  let(:seller) do
+    User.create!(
+      username: 'seller',
+      password: 'password',
+      password_confirmation: 'password',
+      email: 'seller_email@test.com',
+      phone_number: '1234567890'
+    )
+  end
+  let(:low_priced_item) { user.items.create!(title: "Low Priced Item", price: 5, description: "Cheap item") }
+  let(:mid_priced_item) { user.items.create!(title: "Mid Priced Item", price: 50, description: "Moderately priced item") }
+  let(:high_priced_item) { user.items.create!(title: "High Priced Item", price: 500, description: "Expensive item") }
+  let(:bookmarked_item) { user.items.create!(title: "Bookmarked Item", price: 10, description: "Bookmarked item description") }
+  let(:non_bookmarked_item) { user.items.create!(title: "Non-Bookmarked Item", price: 20, description: "Non-bookmarked item description") }
+
+  let(:category1) { Category.create!(name: 'Category1') }
+  let(:category2) { Category.create!(name: 'Category2') }
+  let(:item1) { seller.items.create!(title: "Item1", categories: [category1], price: 10, description: "test") }
+  let(:item2) { seller.items.create!(title: "Item2", categories: [category2], price: 20, description: "test") }
+  let(:item3) { seller.items.create!(title: "Item3", categories: [category1], price: 30, description: "test") }
+
+
   describe "Get #index" do
     context "when user is logged in" do
       before do
@@ -94,20 +116,81 @@ RSpec.describe SearchController, type: :controller do
       expect(Item).to receive(:search).with(params[:search_term], params[:categories])
       get :index, :search_term => 'test', :categories => ['1', '2']
     end
-    it 'filters by seller where items are found' do
-      expect(Item).to receive(:search).with('Item', Category.all.map(&:name)).and_return(Item.all)
-      allow(User).to receive(:find_by).with(username: 'testuser').and_return(user.id)
-      expect(Item).to receive(:where).with("items.user_id = ?", user.id).and_return(Item.where(user_id: user.id))
 
-      get :index, params: { search_term: 'Item', seller: 'testuser' }
+    it 'filters by seller where items are found' do
+      # Stub `User.find_by` to return the seller
+      allow(User).to receive(:find_by).with(id: user.id).and_return(user)
+      allow(User).to receive(:find_by).with(username: 'seller').and_return(user)
+      get :index, search_term: 'Item', seller: 'seller'
     end
+
     it 'filters by seller where no items are found' do
-      params = { search_term: 'No Match', seller: 'testuser' }
-      expect(Item).to receive(:search).with(params[:search_term], Category.all.map(&:name))
-      expect(Item).to receive(:where).with("items.user_id = ?", user.id)
-      get :index, :search_term => 'No Match', :seller => 'testuser'
+      nonexistent_seller = 'nonexistent_user'
+      allow(User).to receive(:find_by).with(id: user.id).and_return(user)
+      allow(User).to receive(:find_by).with(username: nonexistent_seller).and_return(nil)
+
+      get :index, params: { search_term: 'No Match', seller: nonexistent_seller }
+
+      expect(assigns(:results)).to be_empty
+    end
+
+    it 'filters by min price' do
+      get :index, min_price: 10, search_term: 'Item'
+      expect(assigns(:results)).to include(mid_priced_item, high_priced_item)
+      expect(assigns(:results)).not_to include(low_priced_item)
+    end
+
+    it 'filters by max price' do
+      get :index, max_price: 100, search_term: 'Item'
+      expect(assigns(:results)).to include(low_priced_item, mid_priced_item)
+      expect(assigns(:results)).not_to include(high_priced_item)
+    end
+
+    it 'returns only bookmarked items when bookmark filter is applied' do
+      user.bookmarks.create!(item: bookmarked_item)
+      get :index, bookmarks: '1', search_term: 'Item'
+      expect(assigns(:results)).to include(bookmarked_item)
+      expect(assigns(:results)).not_to include(non_bookmarked_item)
+    end
+
+    it 'returns all items when bookmark filter is not applied' do
+      get :index
+      expect(assigns(:results)).to include(bookmarked_item, non_bookmarked_item)
+    end
+
+    it 'filters by bookmarks and seller items' do
+      # Stub `User.find_by` to return the seller
+      allow(User).to receive(:find_by).with(id: user.id).and_return(user)
+      allow(User).to receive(:find_by).with(username: 'seller').and_return(user)
+      get :index, search_term: 'Item', seller: 'seller', bookmarks: '1'
     end
   end
+
+  describe "Get #index with combined filters" do
+    before do
+      controller.log_in(user)
+      user.bookmarks.create!(item: item1)
+    end
+
+    it 'applies category and seller filters' do
+      get :index, categories: [category1.name], seller: seller.username, search_term: 'Item'
+      expect(assigns(:results)).to include(item1, item3)
+      expect(assigns(:results)).not_to include(item2)
+    end
+
+    it 'applies price range and bookmark filters' do
+      get :index, min_price: 10, max_price: 25, bookmarks: '1', search_term: 'Item'
+      expect(assigns(:results)).to include(item1)
+      expect(assigns(:results)).not_to include(item2, item3)
+    end
+
+    it 'applies all filters together' do
+      get :index, categories: [category1.name], seller: seller.username, min_price: 5, max_price: 15, bookmarks: '1', search_term: 'Item'
+      expect(assigns(:results)).to include(item1)
+      expect(assigns(:results)).not_to include(item2, item3)
+    end
+  end
+
 end
 
 
