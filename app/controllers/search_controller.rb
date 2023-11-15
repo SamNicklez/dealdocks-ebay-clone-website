8280b789b1fac8fb959b26c0410f7264e74e6cef
Sam Loecke <sloecke@uiowa.edu>
2023-11-14 19:05:12 -0600

    Fixed a mix up with the nav bar that made it so the search term wasn't passed correctly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
 
   # Perform search and display results, with optional category filtering
   def index
+    puts params[:search_term]
     @results = Item.search(params[:search_term], params[:categories])
   end
 end
