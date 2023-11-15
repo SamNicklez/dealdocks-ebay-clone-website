f8fdeae06f9216ef97855b35f99ca7b455e2b8c6
Sam Loecke <sloecke@uiowa.edu>
2023-11-15 00:33:25 -0600

    Added filtering by my bookmarks, redid the controller for filtering with bookmarked items and categories

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,23 +3,25 @@ class SearchController < ApplicationController
 
   # Perform search and display results, with optional category filtering
   def index
-    if params[:bookmarks].present?
-      # only get the users bookmarked items
+    # Start with all items or just bookmarked items based on the bookmarks param
+    if params[:bookmarks].present? and params[:bookmarks] == '1'
+      puts "bookmarks param is true"
+      # Only get the user's bookmarked items
       @results = current_user.bookmarked_items
     else
-      # set up the min and max price for the price range
-      if params[:categories].nil? or params[:categories].empty?
-        @results = Item.search params[:search_term], Category.all.map(&:name)
-      else
-        @results = Item.search(params[:search_term], params[:categories])
-      end
-
-      if params[:min_price].present?
-        @results = @results.where("price >= ?", params[:min_price])
-      end
-      if params[:max_price].present?
-        @results = @results.where("price <= ?", params[:max_price])
-      end
+      @results = Item.all
     end
+
+    # Filter by categories if any are selected
+    if params[:categories].present?
+      @results = @results.joins(:categories).where(categories: { name: params[:categories] })
+      @results = @results.search(params[:search_term], params[:categories])
+    else
+      @results = @results.search(params[:search_term], Category.all.map(&:name))
+    end
+
+    # Filter by price range if specified
+    @results = @results.where("price >= ?", params[:min_price]) if params[:min_price].present?
+    @results = @results.where("price <= ?", params[:max_price]) if params[:max_price].present?
   end
 end
