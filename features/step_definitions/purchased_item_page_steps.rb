ea4fb50b07ce18a961500028cd99a79eea187f65
Luke Farmer <ljfarmer@uiowa.edu>
2023-12-01 10:37:57 -0600

    Fixed Cucumber tests

--- features/step_definitions/purchased_item_page_steps.rb
+++ features/step_definitions/purchased_item_page_steps.rb
@@ -10,7 +10,7 @@ end
 
 And(/^I have valid payment methods and addresses$/) do
   @user.addresses.create!(shipping_address_1: "19 E Burlington St", shipping_address_2: "Apt 4", city: "Anytown", state: "State", country: "Country", postal_code: "12345")
-  @user.payment_methods.create!(encrypted_card_number: "encrypted_card_number", encrypted_card_number_iv: "encrypted_card_number_iv", expiration_date: Date.new(2025, 01, 01).to_s(:long))
+  @user.payment_methods.create!(encrypted_card_number: "encrypted_card_number", expiration_date: Date.new(2025, 01, 01).to_s(:long))
 end
 
 Then(/^I should see the purchased item page:$/) do |table|
