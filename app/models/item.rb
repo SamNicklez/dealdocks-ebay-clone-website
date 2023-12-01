cec5461fe828bf36c820093bf958f1e5e0907676
Sam Loecke <sloecke@uiowa.edu>
2023-12-01 13:36:01 -0600

    Finished the cucumber test to test the edit item functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -73,16 +73,17 @@ class Item < ApplicationRecord
     # Update categories
     self.categories = Category.where(id: category_ids.reject(&:blank?))
 
-    # Handle images
-    images.each do |uploaded_image|
-      next unless uploaded_image.respond_to?(:tempfile)
-      image_file_path = uploaded_image.tempfile.path
-      image = MiniMagick::Image.new(image_file_path)
-      image.resize('256x256')
-      image_type, image_data = Image.get_image_data(image_file_path)
-      self.images.create!(data: image_data, image_type: image_type)
+    if images.present?
+      # Handle images
+      images.each do |uploaded_image|
+        next unless uploaded_image.respond_to?(:tempfile)
+        image_file_path = uploaded_image.tempfile.path
+        image = MiniMagick::Image.new(image_file_path)
+        image.resize('256x256')
+        image_type, image_data = Image.get_image_data(image_file_path)
+        self.images.create!(data: image_data, image_type: image_type)
+      end
     end
-
     self
   end
 
--- features/edit_item.feature
+++ features/edit_item.feature
@@ -1,6 +1,6 @@
 Feature: Editing an existing listed item
   Background:
-    Given I am logged in as "Item_editer"
+    Given I am logged in as "edit_user"
     Given there are categories created
     Given I have the following items for sale:
       | title       | description  | categories    | price    |
@@ -8,3 +8,10 @@ Feature: Editing an existing listed item
 
   Scenario: Editing an item
     Given I search for my items
+    Given I click on the item link
+    And I click the "Edit Item" button
+    And I edit the item as the following:
+      | title          | description  | categories    | price    |
+      | Chevy Truck    | Small Truck  | Electronics   | 10.00    |
+    Then I should see the item page for "Chevy Truck"
+
