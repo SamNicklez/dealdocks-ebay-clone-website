1e30f5f747cc144733397e59c26111923491e37c
Sam Loecke <sloecke@uiowa.edu>
2023-12-06 12:43:59 -0600

    Created the create method for creating a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,12 +4,13 @@ class ReviewsController < ApplicationController
   before_filter :find_review, only: [:show, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update, :destroy]
 
+
   # validations
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :content, presence: true, length: { maximum: 500 }
-  validates :reviewer_id, presence: true
-  validates :seller_id, presence: true
-  validates :item_id, presence: true
+  # validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  # validates :content, presence: true, length: { maximum: 500 }
+  # validates :reviewer_id, presence: true
+  # validates :seller_id, presence: true
+  # validates :item_id, presence: true
 
   def index
 
@@ -21,6 +22,30 @@ class ReviewsController < ApplicationController
 
   def create
 
+    @review = Review.new(
+      rating: params[:review][:rating],
+      content: params[:review][:content],
+      reviewer_id: params[:review][:reviewer_id],
+      seller_id: params[:review][:seller_id],
+      item_id: params[:review][:item_id]
+    )
+
+    # Assuming current_user is the one who made the purchase
+    purchase = Purchase.find_by(item_id: params[:review][:item_id], user: current_user)
+
+    if purchase && !purchase.reviewed?
+      @review.purchase = purchase
+      if @review.save
+        flash[:notice] = "Review was saved"
+        redirect_to item_path(@review.item_id)
+      else
+        flash[:error] = "Review could not be saved"
+        redirect_to item_path(@review.item_id)
+      end
+    else
+      flash[:alert] = "You have already reviewed this item or the purchase does not exist."
+      redirect_to item_path(@review.item_id)
+    end
   end
 
   def show
