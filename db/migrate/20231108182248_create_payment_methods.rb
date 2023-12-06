25abf50efeb33f3d81baf44dd104e573330e9b38
ColeArduser <carduser@uiowa.edu>
2023-12-05 21:23:20 -0600

    bugfix migration error

--- db/migrate/20231108182248_create_payment_methods.rb
+++ db/migrate/20231108182248_create_payment_methods.rb
@@ -2,7 +2,7 @@ class CreatePaymentMethods < ActiveRecord::Migration
   def change
     create_table :payment_methods do |t|
       t.string :encrypted_card_number
-      t.date :expiration_date
+      t.string :expiration_date
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,59 +14,59 @@
 ActiveRecord::Schema.define(version: 20231204163211) do
 
   create_table "addresses", force: :cascade do |t|
-    t.string "shipping_address_1"
-    t.string "shipping_address_2"
-    t.string "city"
-    t.string "state"
-    t.string "country"
-    t.string "postal_code"
+    t.string   "shipping_address_1"
+    t.string   "shipping_address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "postal_code"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_id"
+    t.integer  "user_id"
   end
 
   add_index "addresses", ["user_id"], name: "index_addresses_on_user_id"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "bookmarks", ["item_id"], name: "index_bookmarks_on_item_id"
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "images", force: :cascade do |t|
-    t.binary "data"
+    t.binary   "data"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "item_id"
-    t.string "image_type"
+    t.integer  "item_id"
+    t.string   "image_type"
   end
 
   add_index "images", ["item_id"], name: "index_images_on_item_id"
 
   create_table "items", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string   "title",                                    null: false
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_id"
-    t.decimal "price", precision: 10, scale: 2
-    t.float "length"
-    t.float "width"
-    t.float "height"
-    t.string "dimension_units"
-    t.float "weight"
-    t.string "weight_units"
-    t.integer "condition" # 0 = New, 1 = Very Good, 2 = Good, 3 = Acceptable, 4 = Poor
+    t.integer  "user_id"
+    t.decimal  "price",           precision: 10, scale: 2
+    t.float    "length"
+    t.float    "width"
+    t.float    "height"
+    t.string   "dimension_units"
+    t.float    "weight"
+    t.string   "weight_units"
+    t.integer  "condition"
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
@@ -80,22 +80,22 @@ ActiveRecord::Schema.define(version: 20231204163211) do
   add_index "items_categories", ["item_id"], name: "index_items_categories_on_item_id"
 
   create_table "payment_methods", force: :cascade do |t|
-    t.string "encrypted_card_number"
-    t.string "expiration_date"
+    t.string   "encrypted_card_number"
+    t.string   "expiration_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_id"
+    t.integer  "user_id"
   end
 
   add_index "payment_methods", ["user_id"], name: "index_payment_methods_on_user_id"
 
   create_table "purchases", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "address_id"
-    t.integer "payment_method_id"
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "address_id"
+    t.integer  "payment_method_id"
   end
 
   add_index "purchases", ["address_id"], name: "index_purchases_on_address_id"
@@ -103,14 +103,14 @@ ActiveRecord::Schema.define(version: 20231204163211) do
   add_index "purchases", ["payment_method_id"], name: "index_purchases_on_payment_method_id"
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "phone_number"
+    t.string   "username",      null: false
+    t.string   "email",         null: false
+    t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "uid"
-    t.string "provider"
-    t.string "session_token"
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "session_token"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
