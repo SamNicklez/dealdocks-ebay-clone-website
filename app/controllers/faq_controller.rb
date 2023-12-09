f606de563b66319f2de5b12c0979fb16be290419
Luke Farmer <ljfarmer@uiowa.edu>
2023-12-09 13:59:41 -0600

    Changed FAQ to only show answers when a question is pressed

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -4,7 +4,11 @@ class FaqController < ApplicationController
 
   def show
     # @item is already set by set_item
-
+    @faq_questions = [
+      { question: "How do I sign up?", answer: "Click the Sign Up button on the homepage to create an account." },
+      { question: "How do I change my password?", answer: "Go to your account settings and select Change Password." },
+    # More FAQs
+    ]
   end
 
 end
