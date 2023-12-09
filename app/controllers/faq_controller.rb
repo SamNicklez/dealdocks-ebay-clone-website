519bd0216aadc7cecfab0808a5974233887178ee
Luke Farmer <ljfarmer@uiowa.edu>
2023-12-09 13:41:24 -0600

    Set up routes to a basic FAQ page

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,12 +1,10 @@
-class FAQController < ApplicationController
+class FaqController < ApplicationController
   before_action :set_current_user
 
 
   def show
     # @item is already set by set_item
-    @user = current_user
-    @item
-    @seller = User.find(@item.user_id)
+
   end
 
 end
