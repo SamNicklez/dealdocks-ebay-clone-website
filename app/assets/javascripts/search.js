e6fa6c16e1a39df825e99975c41c770a2afcf6f8
SamNicklez <snicklaus@uiowa.edu>
2023-12-08 19:25:34 -0600

    added css to search page, added javascript to hide and unhide the filters, added css on homepage for box shadows

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById('toggleButton');
+  var form = document.getElementById('dropdownForm');
+
+  button.addEventListener('click', function() {
+    if (form.style.display === "none") {
+      form.style.display = "block";
+      button.textContent = "Hide Filters";
+    } else {
+      form.style.display = "none";
+      button.textContent = "Show Filters";
+    }
+  });
+});
