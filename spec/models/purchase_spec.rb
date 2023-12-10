3a14005276e408ffdf9d699a91eb6f8236e1a44b
Luke Farmer <ljfarmer@uiowa.edu>
2023-12-10 01:01:33 -0600

    Added tests for Purchase

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  let(:purchase) { Purchase.new }
+
+
+
+  describe 'reviewed' do
+    it 'should return true if the purchase has been reviewed' do
+      purchase.review = Review.new
+      expect(purchase.reviewed?).to eq(true)
+    end
+  end
 end
